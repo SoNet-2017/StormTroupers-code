aaedc9bda8c3bb9dbc406ebf6766d17c4ec50713
giancarloavalle <s233534@studenti.polito.it>
2017-06-09 12:55:03 +0200

    Continuato editProfile

--- app/homePageView/homePageView.js
+++ app/homePageView/homePageView.js
@@ -70,6 +70,7 @@ angular.module('myApp.homePageView', ['ngRoute'])
                     document.getElementById("userRolesHome").innerHTML+=", ";
                 }
             }
+
             $scope.filterSearch={};
 
             var length=$scope.filterUsers.length;
